d36a4cfb2c130478e219a259e165f8b29177be26
Griffin McCormick <mccormickgriffin@gmail.com>
2023-10-07 20:33:49 -0400

    added database connection

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const sequelize = require("./database/connection");
+
+
+async function testDatabaseConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
 
 const appName = process.env.APP_NAME;
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`${appName} listening on port ${port}`);
+  testDatabaseConnection();
 });
